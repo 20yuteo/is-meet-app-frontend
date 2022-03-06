2e78bac5871e26226788566926b2dfe4e37e14da
arakakitatsuki <20yuteo@gmail.com>
2022-03-06 15:16:01 +0900

    ローダーを追加

---
+++ src/components/atomic/Loader.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-overlay
+   v-model="isLoading"
+   class="align-center justify-center"
+  >
+    <v-progress-circular
+      indeterminate
+      size="64"
+    ></v-progress-circular>
+  </v-overlay>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, watchEffect } from 'vue'
+import { useStore } from '@/store/index'
+
+export default defineComponent({
+  name: 'Loader',
+
+  setup () {
+    const store = useStore()
+
+    const isLoading = ref<boolean>(false)
+
+    watchEffect(() => {
+      isLoading.value = store.state.loader.isLoading
+    })
+
+    return {
+      isLoading
+    }
+  }
+})
+</script>
