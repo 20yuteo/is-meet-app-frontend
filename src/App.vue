812f9a69110807a42b1e4b77bdf0afa2e4fb63b9
arakakitatsuki <20yuteo@gmail.com>
2022-03-06 15:22:11 +0900

    App.vueの修正

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <v-app>
     <Header/>
+    <Alert/>
+    <Loader/>
     <v-main>
       <router-view/>
     </v-main>
@@ -12,11 +14,16 @@
 import { defineComponent } from 'vue'
 import Header from './components/layouts/Header.vue'
 import Footer from './components/layouts/Footer.vue'
+import Alert from './components/atomic/Alert.vue'
+import Loader from './components/atomic/Loader.vue'
+
 export default defineComponent({
   name: 'App',
   components: {
     Header,
-    Footer
+    Footer,
+    Alert,
+    Loader
   }
 })
 </script>
