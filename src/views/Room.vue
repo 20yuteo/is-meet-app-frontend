55377725434fc5d0721a8f3be1067c27db8c1069
arakakitatsuki <20yuteo@gmail.com>
2022-03-30 22:53:36 +0900

    Room画面 追加

---
+++ src/views/Room.vue
@@ -0,0 +1,59 @@
+<template>
+  <skyway-video
+    :room-name="roomName"
+    :user-id="userId"
+    :user-name="userName"
+    :token="token"
+  >
+  </skyway-video>
+</template>
+<script lang="ts">
+import { defineComponent, onBeforeMount, ref } from 'vue'
+import VideoRoom from '@/components/VideoRoom.vue'
+import SkywayVideo from '@/components/SkywayVideo.vue'
+import { useStore } from '@/store/index'
+import { useRoute } from 'vue-router'
+
+export default defineComponent({
+  name: 'Room',
+
+  components: {
+    SkywayVideo
+  },
+
+  setup () {
+    const store = useStore()
+
+    const route = useRoute()
+
+    const roomName = ref<string>('')
+
+    const userName = ref<string>('')
+
+    const userId = ref<string>('')
+
+    const token = ref<string>('')
+
+    onBeforeMount(async () => {
+      if (route.query === undefined) return
+      roomName.value = String(route.query.name)
+      userName.value = String(route.query.user_name)
+      userId.value = String(route.query.member_id)
+      token.value = String(route.query.token)
+      if (!store.state.user.isLogin) return
+      userName.value = String(store.state.user.name)
+      userId.value = String(store.state.user.id)
+    })
+
+    return {
+      roomName,
+      userName,
+      userId,
+      token
+    }
+  }
+})
+</script>
+<style lang="scss" scoped>
+// .skyway-video
+</style>
